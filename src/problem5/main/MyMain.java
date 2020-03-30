0b7126139e49d829a349b5e999ccad0cc2b4ea04
Rahul sharma <rahul.sharma1_cs18@gla.ac.in>
2020-03-30 17:37:42 +0530

    main class

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -6,5 +6,33 @@
  */
 package problem5.main;
 //executable class
+
+import problem5.circularqueue.MyCircularQueue;
+import problem5.student.Student;
+
 public class MyMain {
+    public static void main(String[] args) {
+        //Let us create a list of students
+        ArrayList<Student> studentList = new ArrayList<Student>();
+        //hard coding some student value
+        studentList.add(new Student(1, "A", 1, 0));
+        studentList.add(new Student(2, "B", 2, 2));
+        studentList.add(new Student(3, "C", 1, 1));
+        studentList.add(new Student(4, "D", 2, 2));
+        studentList.add(new Student(5, "E", 0, 0));
+        studentList.add(new Student(6, "F", 2, 1));
+        studentList.add(new Student(7, "G", 1, 1));
+        studentList.add(new Student(8, "H", 2, 1));
+        studentList.add(new Student(9, "I", 0, 0));
+        studentList.add(new Student(10, "J", 0, 0));
+        studentList.add(new Student(11, "K", 2, 1));
+        //creating a queue
+        MyCircularQueue studentqueue = new MyCircularQueue();
+        studentqueue.enqueue(studentList);
+        //printing queue
+        // A,F,H,K should be present in the queue
+        studentqueue.printQueue();
+
+
+    }
 }

c083a7ae2bcbfe77c2ad792a428544dae732d891
Rahul sharma <rahul.sharma1_cs18@gla.ac.in>
2020-03-30 17:15:59 +0530

    implemented binary search tree

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -1,1 +1,72 @@
+package problem1.mybst;
+
 import problem1.node.TreeNode;
+
+
+public class MyBinarySearchTree {
+    public static StringBuilder preOrderString;
+    public static int noLeftCounter = 0;
+    static int levels = 0;
+    public TreeNode root;
+
+    public MyBinarySearchTree() {
+        this.root = null;
+        preOrderString = new StringBuilder();
+    }
+
+    public static void preOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        preOrderString.append(root.getKey() + " ");
+        preOrder(root.getLeft());
+        preOrder(root.getRight());
+    }
+
+    public void add(final int value) {
+        this.root = addService(this.root, value);
+    }
+
+    public TreeNode addService(final TreeNode treeRoot, final int addingValue) {
+        if (treeRoot == null) {
+            return new TreeNode(addingValue);
+        }
+        if (addingValue < treeRoot.getKey()) {
+            treeRoot.setLeft(addService(treeRoot.getLeft(), addingValue));
+        } else if (addingValue == treeRoot.getKey()) {
+            return treeRoot;
+        } else {
+            treeRoot.setRight(addService(treeRoot.getRight(), addingValue));
+        }
+
+        return treeRoot;
+    }
+
+    void printLeftChild(TreeNode treeNode, int level) {
+        if (treeNode == null) {
+            return;
+        }
+        if (levels < level) {
+            if (treeNode.getLeft() == null) {
+                noLeftCounter++;
+            }
+            System.out.print(" " + treeNode.getKey());
+            levels = level;
+        }
+        printLeftChild(treeNode.getLeft(), level + 1);
+        printLeftChild(treeNode.getRight(), level + 1);
+    }
+
+    public void printLeftChild() {
+        printLeftChild(this.root, 1);
+    }
+
+    public void postOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        postOrder(root.getLeft());
+        postOrder(root.getRight());
+        System.out.print(root.getKey() + " ");
+    }
+}
